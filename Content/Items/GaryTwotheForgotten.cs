9f2cf4a91723d32741d6ed2c8456f2c595a053d7
TenkaAmakusa <TerraformedTerra@gmail.com>
2025-05-10 21:34:11 -0400

    Initial

---
+++ Content/Items/GaryTwotheForgotten.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace COOMod.Content.Items
+{ 
+	// This is a basic item template.
+	// Please see tModLoader's ExampleMod for every other example:
+	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+	public class GaryTwotheForgotten : ModItem
+	{
+		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.COOMod.hjson' file.
+		public override void SetDefaults()
+		{
+			Item.damage = 500;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 20;
+			Item.height = 20;
+			Item.useTime = 10;
+			Item.useAnimation = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 6;
+			Item.value = Item.buyPrice(silver: 1);
+			Item.rare = ItemRarityID.Blue;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+			Item.crit = 60;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
